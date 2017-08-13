e923e3e7fff94c88731260d2ac0b8c5e1c6284c9
LDQ-first <178848890@qq.com>
2017-08-13 23:39:16 +0800

    add code

---
+++ js/app.js
@@ -0,0 +1,59 @@
+(function() {
+    function Player(name, el) {
+        this.name = name
+        this.active = false
+        this.$el = el
+        this.$caret = this.$el.querySelector('.caret')
+    }
+
+    Player.prototype = {
+        render: function() {
+            this.$el.className = name
+        },
+        reset: function(name) {
+            this.name = name
+            this.render()
+        },
+        setActive: function(active) {
+
+        }
+
+
+    }
+
+
+
+    function Square(el){
+        this.$el = el;
+        this.val = 0;
+    }
+
+    Square.prototype = {
+        set: function(name, val) {
+
+        },
+        reset: function() {
+
+        }
+    }
+
+
+
+    function Game(el) {
+
+    }
+
+    Game.prototype = {
+        start: function() {
+
+        }
+    }
+
+
+
+    document.addEventListener('DOMContentLoaded', function() {
+        
+    })
+
+
+})()
